4e1e2f589bf24d7c8778466924117390be613a33
Kathia Mgwaba <mgwaba68@gmail.com>
2020-05-13 21:19:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,26 @@
 class Song
     attr_accessor :name, :artist, :genre
     @@count = 0
+    @@artists = []
+    @@genres = []
+    
 
-    def initialize
+    def initialize (song_name, artist, genre)
+      @name = song_name
+      @artist = artist
+      @genre = genre
       @@count += 1
+      @@artists << artists
+      @@genres << genre
     end
+    def self.count
+      @@count
+    end
+
+    def self.artists
+      @@artists.uniq
+    end
+
+    
 
 end
